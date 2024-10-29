90950141eb182698ea429fda4ed93baf830f0b69
“AAndrews-1982” <altonandrews@icloud.com>
2024-10-29 10:37:49 -0500

    fixed GET handler in route file

--- app/api/activities/route.ts
+++ app/api/activities/route.ts
@@ -9,8 +9,8 @@ export const GET = auth(async (req: NextRequest) => {
   const params = req.nextUrl.searchParams;
   const page = params.get("page") ? Number(params.get("page")) : 1;
 
-  //@ts-ignore
-  if (!req.auth) {
+
+  if (!req.auth || !req.auth.user) {
     return NextResponse.json(
       { error: "Unauthorized - Not logged in" },
       { status: 401 }
@@ -18,7 +18,7 @@ export const GET = auth(async (req: NextRequest) => {
   }
 
   const {
-    user: { email }, //@ts-ignore
+    user: { email } = req.auth.user
   } = req.auth;
 
   const activities = await fetchActivities(page, email);
